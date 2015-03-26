8196f68a6021800452ad49be5a66db60cad993bb
Marc LaRocco <Cutmerock@gmail.com>
2015-03-25 20:26:18 -0400

    Yahtzee
    Yahtzee game

---
+++ Yahtzee/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Yahtzee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.picDi1 = new System.Windows.Forms.PictureBox();
+            this.picDi2 = new System.Windows.Forms.PictureBox();
+            this.picDi3 = new System.Windows.Forms.PictureBox();
+            this.picDi4 = new System.Windows.Forms.PictureBox();
+            this.picDi5 = new System.Windows.Forms.PictureBox();
+            this.btnRoll = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picDi1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDi2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDi3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDi4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDi5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picDi1
+            // 
+            this.picDi1.Image = ((System.Drawing.Image)(resources.GetObject("picDi1.Image")));
+            this.picDi1.InitialImage = ((System.Drawing.Image)(resources.GetObject("picDi1.InitialImage")));
+            this.picDi1.Location = new System.Drawing.Point(65, 51);
+            this.picDi1.Name = "picDi1";
+            this.picDi1.Size = new System.Drawing.Size(68, 65);
+            this.picDi1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picDi1.TabIndex = 0;
+            this.picDi1.TabStop = false;
+            // 
+            // picDi2
+            // 
+            this.picDi2.Image = ((System.Drawing.Image)(resources.GetObject("picDi2.Image")));
+            this.picDi2.InitialImage = ((System.Drawing.Image)(resources.GetObject("picDi2.InitialImage")));
+            this.picDi2.Location = new System.Drawing.Point(161, 51);
+            this.picDi2.Name = "picDi2";
+            this.picDi2.Size = new System.Drawing.Size(68, 65);
+            this.picDi2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picDi2.TabIndex = 1;
+            this.picDi2.TabStop = false;
+            // 
+            // picDi3
+            // 
+            this.picDi3.Image = ((System.Drawing.Image)(resources.GetObject("picDi3.Image")));
+            this.picDi3.InitialImage = ((System.Drawing.Image)(resources.GetObject("picDi3.InitialImage")));
+            this.picDi3.Location = new System.Drawing.Point(269, 51);
+            this.picDi3.Name = "picDi3";
+            this.picDi3.Size = new System.Drawing.Size(68, 65);
+            this.picDi3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picDi3.TabIndex = 2;
+            this.picDi3.TabStop = false;
+            // 
+            // picDi4
+            // 
+            this.picDi4.Image = ((System.Drawing.Image)(resources.GetObject("picDi4.Image")));
+            this.picDi4.InitialImage = ((System.Drawing.Image)(resources.GetObject("picDi4.InitialImage")));
+            this.picDi4.Location = new System.Drawing.Point(383, 51);
+            this.picDi4.Name = "picDi4";
+            this.picDi4.Size = new System.Drawing.Size(68, 65);
+            this.picDi4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picDi4.TabIndex = 3;
+            this.picDi4.TabStop = false;
+            // 
+            // picDi5
+            // 
+            this.picDi5.Image = ((System.Drawing.Image)(resources.GetObject("picDi5.Image")));
+            this.picDi5.InitialImage = ((System.Drawing.Image)(resources.GetObject("picDi5.InitialImage")));
+            this.picDi5.Location = new System.Drawing.Point(512, 51);
+            this.picDi5.Name = "picDi5";
+            this.picDi5.Size = new System.Drawing.Size(68, 65);
+            this.picDi5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picDi5.TabIndex = 4;
+            this.picDi5.TabStop = false;
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRoll.Location = new System.Drawing.Point(198, 249);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(215, 34);
+            this.btnRoll.TabIndex = 5;
+            this.btnRoll.Text = "Roll";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 480);
+            this.Controls.Add(this.btnRoll);
+            this.Controls.Add(this.picDi5);
+            this.Controls.Add(this.picDi4);
+            this.Controls.Add(this.picDi3);
+            this.Controls.Add(this.picDi2);
+            this.Controls.Add(this.picDi1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picDi1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDi2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDi3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDi4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picDi5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picDi1;
+        private System.Windows.Forms.PictureBox picDi2;
+        private System.Windows.Forms.PictureBox picDi3;
+        private System.Windows.Forms.PictureBox picDi4;
+        private System.Windows.Forms.PictureBox picDi5;
+        private System.Windows.Forms.Button btnRoll;
+    }
+}
+
